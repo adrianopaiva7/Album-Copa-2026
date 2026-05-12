4eb11e6c980ba4c4e7cddaf21968c1d3a74da470
adrianopaiva7 <adrianojpsantos7@gmail.com>
2026-05-12 09:48:37 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
- const supabaseUrl = 'https://nqyrhkkshfrrelnnjmcv.supabase.co/rest/v1/';
+ const supabaseUrl = 'https://nqyrhkkshfrrelnnjmcv.supabase.co';
 
-const supabaseKey = 'sb_publishable_dnl7In0JZxWhTb43tZrq_g_StxArwj6';
+ const supabaseKey = 'sb_publishable_dnl7In0JZxWhTb43tZrq_g_StxArwj6';
 
-const supabase = window.supabase.createClient(
-  supabaseUrl,
-  supabaseKey
- 
+const supabaseClient = supabase.createClient(
+    supabaseUrl,
+    supabaseKey
+);
+
 const numeros = document.querySelectorAll('.numero');
 
 numeros.forEach(numero => {
@@ -14,24 +15,24 @@ numeros.forEach(numero => {
 
         numero.classList.toggle('marcado');
 
-        const pais =
-          numero.parentElement
-          .querySelector('.pais')
-          ?.innerText;
+        const pais = numero
+            .parentElement
+            .querySelector('.pais')
+            ?.innerText;
 
         const numeroTexto = numero.innerText;
 
-        await supabase
-          .from('marcacoes')
-          .insert({
+        const { error } = await supabaseClient
+            .from('marcacoes')
+            .insert([
+                {
+                    pais: pais,
+                    numero: numeroTexto,
+                    marcado: true
+                }
+            ]);
 
-              pais: pais,
-
-              numero: numeroTexto,
-
-              marcado: true
-
-          });
+        console.log(error);
 
     });
 
